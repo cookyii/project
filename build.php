be46f64a591c4c169ea887a7fd696a362ccfb808
Revin Roman <xgismox@gmail.com>
2015-09-13 12:57:35 +0300

    Update

--- build.php
+++ build.php
@@ -126,6 +126,16 @@ $config = [
             'commandline' => './frontend rbac/update',
         ],
     ],
+
+    'extract' => [
+        '.description' => 'Extract codebase to split repos',
+        '.depends' => [
+            'clear',
+        ],
+        '.task' => [
+            'class' => 'dev\build\ExtractTask',
+        ],
+    ],
 ];
 
 // create applications tasks
