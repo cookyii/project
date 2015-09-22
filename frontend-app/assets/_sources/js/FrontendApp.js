cbcfe4f8ddebe2c09fa9112af7d9c13c2773e4b6
Revin Roman <xgismox@gmail.com>
2015-09-22 23:09:15 +0300

    Update

--- frontend-app/assets/_sources/js/FrontendApp.js
+++ frontend-app/assets/_sources/js/FrontendApp.js
@@ -3,21 +3,30 @@
 
   ng.module('FrontendApp', [
     'ngCookies', 'ngSanitize',
-    'ngMaterial',
     'ui.bootstrap',
     'angular-loading-bar',
     'filters'
   ])
 
     .config([
-      '$httpProvider', '$animateProvider', '$mdThemingProvider',
-      function ($httpProvider, $animateProvider, $mdThemingProvider) {
+      '$httpProvider', '$animateProvider', 'redactorOptions',
+      function ($httpProvider, $animateProvider, redactorOptions) {
         $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
         $httpProvider.defaults.headers.common['X-CSRF-Token'] = yii.getCsrfToken();
 
         $animateProvider.classNameFilter(/^(?:(?!wo-animate).)*$/);
 
-        $mdThemingProvider.theme('default');
+        redactorOptions.plugins = ['filemanager', 'imagemanager', 'fullscreen'];
+        redactorOptions.minHeight = 200;
+        redactorOptions.buttons = [
+          'html', 'formatting',
+          'bold', 'italic', 'deleted',
+          'unorderedlist', 'orderedlist',
+          'outdent', 'indent',
+          'image', 'file', 'link',
+          'alignment',
+          'horizontalrule'
+        ];
       }
     ])
 
